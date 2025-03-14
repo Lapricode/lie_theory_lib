e4cd530588415f67ae4b846af561f641ae51348f
Lapricode <prilampros@gmail.com>
2025-03-14 17:18:49 +0200

    adding functionality for 2d rotation groups

--- S1_rotation.py
+++ S1_rotation.py
@@ -6,6 +6,32 @@ def group_element(theta):
     return z
 
 def algebra_element(theta):
-    z = 1j * theta
-    return z
+    tau_hat = 1j * theta
+    return tau_hat
 
+def hat(theta):
+    return 1j * theta
+
+def vee(z):
+    return z.imag
+
+def group_action(z, w):
+    return z * w
+
+def Exp(theta):
+    return np.exp(1j * theta)  # np.exp(1j * theta) = np.cos(theta) + 1j * np.sin(theta)
+
+def Log(z):
+    return np.angle(z)  # np.angle(z) = np.arctan2(z.imag, z.real)
+
+def plus_right(z, tau):
+    return z * Exp(tau)
+
+def plus_left(z, tau):
+    return Exp(tau) * z
+
+def minus_right(z, w):
+    return Log(w.conjugate() * z)
+
+def minus_left(z, w):
+    return Log(z * w.conjugate())
--- SO2_rotation.py
+++ SO2_rotation.py
@@ -1,3 +1,26 @@
 import numpy as np
 
 
+def group_element(theta):
+    R = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+    return R
+
+def algebra_element(theta):
+    tau_hat = np.array([[0, -theta], [theta, 0]])
+    return tau_hat
+
+def hat(theta):
+    return np.array([[0, -theta], [theta, 0]])
+
+def vee(R):
+    return R[1, 0]
+
+def group_action(R, w):
+    return R @ w
+
+def Exp(theta):
+    return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+
+def Log(R):
+    return np.arctan2(R[1, 0], R[0, 0])
+
