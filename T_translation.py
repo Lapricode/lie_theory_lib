4368dc4eba3963d98ec68c436070e8dee1305dfd
Lapricode <prilampros@gmail.com>
2025-03-17 02:28:03 +0200

    adding functionality for translational group

--- T_translation.py
+++ T_translation.py
@@ -1,3 +1,29 @@
 import numpy as np
 
 
+'''  '''
+
+def group_element():
+    pass
+
+def group_composition():
+    pass
+
+def group_inverse():
+    pass
+
+def group_action():
+    pass
+
+def algebra_element():
+    pass
+
+def hat():
+    pass
+
+def vee():
+    pass
+
+
+def testing():
+    pass
